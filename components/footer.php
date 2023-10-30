5d03e3b48e0fdebfac70dd17807b4a34620301f0
Ilyas Amran <muhammadilyasamran@gmail.com>
2023-10-30 23:58:44 +0800

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:01111047614"><i class="fas fa-phone"></i> +011-1104 7614(Encik Ilyas)</a>
+         <a href="tel:01127386775"><i class="fas fa-phone"></i> +011-2738 6775(Cik Nazatul)</a>
+         <a href="tel:013-522 8924"><i class="fas fa-phone"></i> +013-522 8924(Encik Aidil)</a>
+         <a href="tel:0134635991"><i class="fas fa-phone"></i> +013-463 5991(Cik Amirah)</a>
+          
+         <a href="mailto:muhammadilyasamran@gmail.com"><i class="fas fa-envelope"></i> muhammadilyasamran@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Segamat, Malaysia - 85000 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   
+
+</footer>
